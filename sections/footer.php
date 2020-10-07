ba28e5a2e1b4103277cf2947b165f956e5b7ccc2
LouiseThomsson <louise.thomsson@netrelations.com>
2020-10-07 14:18:35 +0200

    new
    support

---
+++ sections/footer.php
@@ -0,0 +1,27 @@
+<?php 
+
+?>
+
+<!-- Footer -->
+<footer id="myFooter">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-10">
+                <h2 class="logo"><h1>Om oss</h1></h2>
+                <div class="row-2">
+                <p>Lorem ipsum lorem </p>
+                </div>
+            </div>
+
+            <div class="col-sm-2">
+                <div class="social-networks">
+                    <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+                    <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+                    <a href="#" class="google"><i class="fa fa-google-plus"></i></a>
+                </div>
+                <a href="#"><button type="button" class="btn btn-default">Kontakta oss</button></a>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Footer -->
---
+++ sections/header.php
@@ -0,0 +1,51 @@
+<?php 
+
+?>
+
+
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link rel="stylesheet" href="head_foot.css">
+    <link href="https://fonts.googleapis.com/css?family=Jua" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  </head>
+  
+  <body>
+
+<!-- Navbars -->
+
+
+    <nav class="navbar sticky-top navbar1">
+    <a class="navbar-brand" href="#">Hair Spray</a>
+        <div class="container-sm">
+            <button type="button" class="btn btn-default">Inlogg</button>  
+            <button type="button" class="btn btn-default">Skapa Konto</button>
+            <button type="button" class="btn btn-default kundvagn"><img src="../hairSpray/bilder/shopping_kart.png"></button>
+            <!-- <input class="form-control mr-sm-2" type="text" placeholder="Username" aria-label="username">
+            <input class="form-control mr-sm-2" type="password" placeholder="Password" aria-label="Password"> -->
+        </div>
+    </nav>
+
+    <nav class="navbar linkproducts nav-justified navbar2">
+      <a class="nav-item nav-link" href="#">Shampoo</a>
+      <a class="nav-item nav-link" href="#">Hårspray</a>
+      <a class="nav-item nav-link" href="#">Vax</a>
+    </nav>
+
+
+<!-- Navbars -->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
